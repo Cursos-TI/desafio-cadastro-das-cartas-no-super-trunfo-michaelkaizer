e722e6cb2a88d9e82af5a5fa9e7134cdc6bec53d
michaelkaizer <gabrielpereiranobrepb@gmail.com>
2025-01-25 18:28:45 +0000

    criei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,6 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    printf("Desafio de trunfo\n");
     return 0;
 }
